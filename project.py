8da391c6dc0dd24ee0f99b2476cf91f2a76d0133
Łukasz Kubicki <lkubicki1986@gmail.com>
2022-10-06 18:44:37 +0200

    change if and else to bool

--- project.py
+++ project.py
@@ -1,10 +1,9 @@
+from re import S
+
+
 def isPalindrom(s):
     return s == s[::-1]
 
 s = input("podaj slowo zobaczymy czy jest to palindrom:")
 ans= isPalindrom(s)
-
-if ans:
-    print("yes")
-else:
-    print("no")
+print(bool(ans))
