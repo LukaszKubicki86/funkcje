ff325403cf4c375512271c7784267bc40dfaba68
Łukasz Kubicki <lkubicki1986@gmail.com>
2022-10-06 18:53:27 +0200

    add comments

--- project.py
+++ project.py
@@ -2,8 +2,8 @@ from re import S
 
 
 def isPalindrom(s):
-    return s == s[::-1]
-
+    return s == s[::-1]  #odczytanie co któregoś elementu listy w tym wypadku co 1 
+# sprawdzanie czy slowo jest palindromem
 s = input("podaj slowo zobaczymy czy jest to palindrom:")
 ans= isPalindrom(s)
 print(bool(ans))
